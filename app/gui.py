6bd41982655ed1c9ebbb53eca3824758bedcb6b2
Pedro Ivo <pedro.ivo.freire.aragao@gmail.com>
2022-09-09 09:45:13 -0300

    Fazendo algumas mudanças no projeto nomeação

---
+++ app/gui.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import PySimpleGUI as sg
+from utils import has_name_in_file
+
+layout = [
+    [
+        sg.Text('Nome do candidato: '),
+        sg.InputText(
+            default_text='Pedro Ivo Freire Aragão', focus=True, key='candidato'
+        ),
+    ],
+    [
+        sg.FileBrowse(
+            'Arquivo',
+            file_types=(('Documents File', '.pdf'),),
+            initial_folder=Path().home() / 'Downloads',
+        ),
+        sg.InputText(key='path', enable_events=True, readonly=True),
+    ],
+    [sg.Button('Buscar')],
+    [sg.Text('', key='saida')],
+]
+
+window = sg.Window(
+    'Busca nome em arquivos pdfs', layout, element_justification='c'
+)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'Buscar':
+        message = ''
+        has_name, page_number = has_name_in_file(
+            values['candidato'], values['path']
+        )
+
+        if not has_name:
+            message = 'Infelizmente seu nome não foi encontrado no documento.'
+        else:
+            message = f'Opa! Seu nome foi encontrado na página {page_number} desse documento.'
+
+        window['saida'].update(message)
+
+window.close()
--- app/main.py
+++ app/main.py
@@ -1,36 +1,118 @@
+import email.message
+import os
+import smtplib
 from pathlib import Path
-import PySimpleGUI as sg
-from utils import has_name_in_file
+from time import sleep
+from typing import Tuple
+from unicodedata import normalize
+from datetime import datetime
 
-layout = [
-    [sg.Text('Nome do candidato: '), sg.InputText(key='candidato')],
-    [
-        sg.FileBrowse('Arquivo', file_types=(('Documents File', '.pdf'),),
-            initial_folder=Path().home(),
-        ),
-        sg.InputText(key='path', enable_events=True, readonly=True),
-    ],
-    [sg.Button('Buscar')],
-    [sg.Text('', key='saida')],
-]
+from dotenv import load_dotenv, find_dotenv
 
-window = sg.Window('Busca Busca nome em arquivos pdfs', layout, element_justification='c')
+from PyPDF2 import PdfReader
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-while True:
-    event, values = window.read()
 
-    if event == sg.WIN_CLOSED:
-        break
+def get_browser(url: str, path: Path = None) -> WebDriver:
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--dns-prefetch-disable')
 
-    if event == 'Buscar':
-        message = ''
-        has_name, page_number = has_name_in_file(values['candidato'], values['path'])
+    if path:
+        path.mkdir(exist_ok=True)
+        prefs = {'download.default_directory': f'{path}'}
+        options.add_experimental_option('prefs', prefs)
 
+    chrome_service = Service(ChromeDriverManager().install())
+    browser = Chrome(service=chrome_service, options=options)
+    browser.get(url)
+    return browser
+
+
+def download_file(url: str, path: Path = None) -> None:
+    browser = get_browser(url, path)
+    sleep(3)
+    browser.find_element(By.ID, 'download').click()
+    sleep(15)
+    browser.quit()
+
+
+def normalize_name(name: str) -> str:
+    normalized = (
+        normalize('NFKD', name).encode('ASCII', 'ignore').decode('ASCII')
+    )
+    return normalized.strip().upper()
+
+
+def has_name_in_file(name: str, filename: str) -> Tuple[bool, int]:
+    path = Path(OUTPUT_DIR, filename)
+    reader = PdfReader(path)
+    name_normalized = normalize_name(name)
+
+    current_page = 1
+    for page in reader.pages:
+        text = page.extract_text()
+        if name_normalized in text:
+            return True, current_page
+        current_page += 1
+
+    return False, 0
+
+
+def rename_file(filename: str) -> str:
+    path = Path(OUTPUT_DIR, filename)
+    old_name = path.stem.split('-')[0]
+    today = datetime.strftime(datetime.now(), '%d-%m-%Y')
+    new_name = f'{old_name}-{today}.pdf'
+    path.rename(Path(path.parent, new_name))
+    return new_name
+
+
+def send_email(message: str) -> None:
+    corpo_email = f"""
+    <h1>Nomeação no Diário da Justíça</h1>
+    <p>{message}</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = 'Nomeação TJ CE'
+    msg['From'] = 'pedro.ivo.freire.aragao@gmail.com'
+    msg['To'] = 'pedro.ivo.freire.aragao@gmail.com'
+    password = os.environ.get('SENHA_EMAIL')
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email)
+
+    server = smtplib.SMTP('smtp.gmail.com: 587')
+    server.starttls()
+    server.login(msg['From'], password)
+    server.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+
+
+OUTPUT_DIR = Path(Path.home(), 'Downloads')
+BASE_URL = 'https://esaj.tjce.jus.br/cdje/index.do'
+
+username = 'Pedro Ivo Freire Aragão'
+
+if __name__ == '__main__':
+    load_dotenv(find_dotenv())
+    download_file(BASE_URL, OUTPUT_DIR)
+    pdfs = [file for file in os.listdir(OUTPUT_DIR) if file.endswith('.pdf')]
+    for pdf in pdfs:
+        has_name, page_number = has_name_in_file(username, pdf)
+        filename = rename_file(pdf)
         if not has_name:
-            message = 'Infelizmente seu nome não foi encontrado no documento.'
+            file = Path(OUTPUT_DIR, filename)
+            os.remove(file)
         else:
-            message = f'Opa! Seu nome foi encontrado na página {page_number} desse documento.'
-
-        window['saida'].update(message)
-
-window.close()
+            send_email(
+                f'Opa! Encontramos o seu nome no arquivo {filename} na página {page_number}'
+            )
